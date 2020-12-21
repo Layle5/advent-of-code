3114572146a805356edc420ea83e9b1ff7e3779f
Louis Dufeu <louis.dufeu@epita.fr>
2020-12-21 23:40:59 +0100

    2020: day-21: add part 2 solution

--- 2020/day-21/src/main.rs
+++ 2020/day-21/src/main.rs
@@ -1,6 +1,6 @@
 use std::fs;
 use std::{
-    collections::{HashMap, HashSet},
+    collections::{BTreeMap, HashMap, HashSet},
     env,
 };
 
@@ -10,10 +10,10 @@ extern crate lazy_static;
 use itertools::Itertools;
 use regex::Regex;
 
-#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
+#[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
 struct Ingredient<'a>(&'a str);
 
-#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
+#[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
 struct Allergen<'a>(&'a str);
 
 #[derive(Debug)]
@@ -50,8 +50,9 @@ fn parse_foods(content: &str) -> Vec<Food> {
         .collect_vec()
 }
 
-fn solve_part_1(content: &str) {
-    let foods = parse_foods(content);
+fn get_allergens_ingredient_map<'a>(
+    foods: &'a [Food],
+) -> BTreeMap<Allergen<'a>, Ingredient<'a>> {
     let allergens: HashSet<Allergen> = foods
         .iter()
         .flat_map(|food| food.allergens.iter().copied())
@@ -69,7 +70,7 @@ fn solve_part_1(content: &str) {
         })
         .collect();
 
-    let allergens_ingredients_map: HashMap<Allergen, HashSet<Ingredient>> =
+    let mut allergens_ingredients_map: HashMap<Allergen, HashSet<Ingredient>> =
         allergens_foods_map
             .iter()
             .map(|(&allergen, foods)| {
@@ -86,20 +87,61 @@ fn solve_part_1(content: &str) {
             })
             .collect();
 
+    let mut allergens_ingredient_map = BTreeMap::new();
+
+    loop {
+        let allergen_ingredients_pair_op = allergens_ingredients_map
+            .iter()
+            .find(|(_, ingredients)| ingredients.len() == 1);
+
+        if allergen_ingredients_pair_op.is_none() {
+            break;
+        }
+
+        let (&allergen, ingredients) = allergen_ingredients_pair_op.unwrap();
+        let ingredient = ingredients.iter().copied().next().unwrap();
+
+        allergens_ingredients_map.remove(&allergen);
+        for (_, ingredients) in allergens_ingredients_map.iter_mut() {
+            ingredients.remove(&ingredient);
+        }
+
+        allergens_ingredient_map.insert(allergen, ingredient);
+    }
+
+    allergens_ingredient_map
+}
+
+fn solve_part_1(content: &str) {
+    let foods = parse_foods(content);
+
+    let allergens_ingredient_map = get_allergens_ingredient_map(&foods);
+
     let count = foods
         .iter()
         .flat_map(|food| food.ingredients.iter())
         .filter(|ingredient| {
-            !allergens_ingredients_map
+            !allergens_ingredient_map
                 .values()
-                .any(|ingredient_set| ingredient_set.contains(ingredient))
+                .any(|other_ingredient| other_ingredient == *ingredient)
         })
         .count();
 
     println!("Part 1: {}", count);
 }
 
-fn solve_part_2(_content: &str) {}
+fn solve_part_2(content: &str) {
+    let foods = parse_foods(content);
+
+    let allergens_ingredient_map = get_allergens_ingredient_map(&foods);
+
+    let ingredients_string = allergens_ingredient_map
+        .values()
+        .map(|ingredient| ingredient.0)
+        .join(",");
+
+    println!("Part 2: {}", ingredients_string);
+}
 
 fn get_content(index: usize, default_filename: &str) -> String {
     let args: Vec<String> = env::args().collect();
